7123e8ec09e498bf14f4e42b4df635e4b591f4c5
Trotsijs <trotsijs@gmail.com>
2023-07-19 20:19:11 +0300

    Add 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>404 Not Found</title>
+    <link rel="shortcut icon" type="image/png" href="https://i.ibb.co/TPmY22Z/q.png">
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</head>
+<body class="min-h-screen bg-gradient-to-br from-purple-950 bg-gray-900 flex items-center justify-center">
+<div class="mx-auto text-center">
+    <img src="/images/error404.png" alt="" class="mx-auto" width="200" height="200">
+    <div class="mx-auto">
+        <x-button class="mt-4">
+            <a href="{{ route('dashboard') }}">Go Back</a>
+        </x-button>
+    </div>
+</div>
+</body>
+</html>
